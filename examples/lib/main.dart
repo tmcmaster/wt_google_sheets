d3c378cbaa1cd62b90a867a3c1c7c9000569128e
Tim McMaster <tim@mcmaster.id.au>
2025-05-23 11:11:37 +1000

    wix_admin packages setup

--- examples/lib/main.dart
+++ examples/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart' hide Route;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wt_google_sheets/wt_google_sheets.dart';
 import 'package:wt_google_sheets_examples/screen/google_sheets_screen.dart';
-import 'package:wt_google_sheets_examples/secrets/flutter-sheets-secrets.dart';
+import 'package:wt_google_sheets_examples/secrets/flutter_sheets_secrets.dart';
 
 void main() {
   runApp(
--- examples/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import package_info_plus
 import path_provider_foundation
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
